1dcf0867a13e4ba0bb54b0e5376377053e8889ee
Dusan <oalfodr@gmail.com>
2020-11-12 05:40:00 +0100

    Reorder elements

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"cpubars",	2,	2},
-	{"",	"memory",	2,	5},	
-	{"",	"volume",	0,	10},
+	{"",	"cpubars",	2,	1},
+	{"",	"memory",	2,	2},
 	{"",	"battery",	5,	3},
 	{"",	"internet",	5,	4},
-	{"",	"clock",	60,	1},
+	{"",	"volume",	0,	5},
+	{"",	"clock",	60,	6},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
