a70683babc828fe07b60eba8935433c06aa37ffc
Dusan <oalfodr@gmail.com>
2020-12-30 21:33:23 +0100

    add music block

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"music",	0,	11},
 	{"",	"cpubars",	2,	1},
 	{"",	"memory",	2,	2},
 	{"",	"battery",	5,	3},
